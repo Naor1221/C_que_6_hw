f46e9f469b0c1de29ea08c78f4966dc250f9a879
Naor Balas <naor56564@walla.com>
2025-12-31 19:38:19 +0200

    Header

---
+++ insert.h
@@ -0,0 +1,5 @@
+#ifndef INSERT_8cff4777c3fb402fb05c81730563cde0
+#define INSERT_8cff4777c3fb402fb05c81730563cde0
+#include <stdio.h>
+void insert_value_to_arr(int arr[],size_t size,int value,int index);
+#endif
