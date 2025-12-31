1a33671c9b8ba34069a177253373cd646fa0593f
Naor Balas <naor56564@walla.com>
2025-12-31 20:58:25 +0200

    update main

--- main.c
+++ main.c
@@ -6,15 +6,17 @@ int main(void) {
     setbuf(stdout, NULL);
     int arr[100]={15,32,26,95,27};
     size_t size=5;
+    size_t *p_len=&size;
     printf("%s\n","Before insert:");
-    for(size_t i=0;i<size;i++){
+    for(size_t i=0;i<*p_len;i++){
         printf("%d ",arr[i]);
     }
-    insert_value_to_arr(arr,size,16,1);
-
-    printf("%s\n","\nAfter insert:");
     size++;
-    for(size_t i=0;i<size;i++){
+    arr_insert(arr,p_len,1,16);
+    
+    printf("%s\n","\nAfter insert:");
+    
+    for(size_t i=0;i<*p_len;i++){
         printf("%d ",arr[i]);
     }
     printf("%c",'\n');
